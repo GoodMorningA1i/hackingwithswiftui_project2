1166e7c8b4ddbdac2605b683244d1b8ce7ddd5f0
Ali Syed <alisyed0206@gmail.com>
2024-09-10 12:12:26 -0400

    added starter files

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ali Syed on 2024-09-10.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
